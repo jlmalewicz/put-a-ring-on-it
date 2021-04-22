4bb9194bd1755d9c989b18c2ccaeb2826cfd684b
Mi Do <50501390+mdoam@users.noreply.github.com>
2021-04-22 10:58:14 -0400

    Update integral.py

--- integral.py
+++ integral.py
@@ -1,7 +1,7 @@
 # integral
 Let $r = \frac{z}{1-z}+r_1 \rightarrow dr = \frac{dz}{(1-z)^2}$. Then, the integral becomes:
-$$\Delta\phi=2\int_{r_1}^{\infty}\frac{dr}{r^2}\left(\frac{1}{b^2}-W_{eff}(r)\right)^{-1/2}=2\int_{0}^{1}\frac{1}{\left(\frac{z}{1-z}+r_1\right)^2}\frac{dz}{(1-z)^2}\left(\frac{1}{b^2}-\frac{1}{\left(\frac{z}{1-z}+r_1\right)^2}+\frac{2M}{\left(\frac{z}{1-z}+r_1\right)^3}\right)^{-1/2}$$
-$$\Delta\phi=2\int_{0}^{1}\frac{dz}{\left[z+r_1(1-z)\right]^2}\left(\frac{1}{b^2}-\frac{1}{\left(\frac{z}{1-z}+r_1\right)^2}+\frac{2M}{\left(\frac{z}{1-z}+r_1\right)^3}\right)^{-1/2}$$
+$$\Delta\phi=2\int_{r_1}^{\infty}\frac{dr}{r^2}\left(\frac{1}{b^2}-W_{eff}(r)\right)^{-1/2}=2\int_{0}^{1}\frac{1}{\left(\frac{z}{1-z}+r_1\right)^2}\frac{dz}{(1-z)^2}\left(\frac{1}{b^2}-\frac{1}{\left(\frac{z}{1-z}+r_1\right)^2}+\frac{2MG}{c^2\left(\frac{z}{1-z}+r_1\right)^3}\right)^{-1/2}$$
+$$\Delta\phi=2\int_{0}^{1}\frac{dz}{\left[z+r_1(1-z)\right]^2}\left(\frac{1}{b^2}-\frac{1}{\left(\frac{z}{1-z}+r_1\right)^2}+\frac{2MG}{c^2\left(\frac{z}{1-z}+r_1\right)^3}\right)^{-1/2}$$
 
 from gaussxw import gaussxwab
 from scipy import constants
@@ -17,7 +17,7 @@ R = 2*G*M/(c**2)
 
 def h(z,r1,b):
     de = z/(1-z)+r1
-    return 2/((z+r1*(1-z))**2) * (1/(b**2) - 1/(de**2) + (2*M)/(de**3))**(-1/2)
+    return 2/((z+r1*(1-z))**2) * (1/(b**2) - 1/(de**2) + (2*M*G)/(c**2)/(de**3))**(-1/2)
         
 lower_lim = 0
 up_lim = 1
