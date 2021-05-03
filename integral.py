7d1e4725e82de1141399fd20c466d207eed7906e
Julie Malewicz <julie.malewicz@gmail.com>
2021-05-03 14:58:24 -0400

    integral update

--- integral.py
+++ integral.py
@@ -1,10 +1,3 @@
-# integral
-$W_{eff}(r)=\frac{1}{r^2}\left(1-\frac{2GM}{rc^2}\right)$
-
-Let $r = \frac{z}{1-z}+r_1 \rightarrow dr = \frac{dz}{(1-z)^2}$. Then, the integral becomes:
-$$\Delta\phi=2\int_{r_1}^{\infty}\frac{dr}{r^2}\left(\frac{1}{b^2}-W_{eff}(r)\right)^{-1/2}=2\int_{0}^{1}\frac{1}{\left(\frac{z}{1-z}+r_1\right)^2}\frac{dz}{(1-z)^2}\left(\frac{1}{b^2}-\frac{1}{\left(\frac{z}{1-z}+r_1\right)^2}+\frac{2MG}{c^2\left(\frac{z}{1-z}+r_1\right)^3}\right)^{-1/2}$$
-$$\Delta\phi=2\int_{0}^{1}\frac{dz}{\left[z+r_1(1-z)\right]^2}\left(\frac{1}{b^2}-\frac{1}{\left(\frac{z}{1-z}+r_1\right)^2}+\frac{2MG}{c^2\left(\frac{z}{1-z}+r_1\right)^3}\right)^{-1/2}$$
-
 from gaussxw import gaussxwab
 from scipy import constants
 
